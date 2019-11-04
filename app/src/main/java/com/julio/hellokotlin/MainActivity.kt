8699d80dd2cd1ac60c0e36d78c5e1ffceb2b9839
Julio Rahman <julio.rahman@gmail.com>
2019-11-04 11:14:52 +0700

    Initial commit

---
+++ app/src/main/java/com/julio/hellokotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.julio.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvHello = findViewById<TextView>(R.id.text_hello)
+        tvHello.text = "Hello Kotlin!"
+    }
+}
